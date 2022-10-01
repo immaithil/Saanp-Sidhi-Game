974d5d38347a849a6188cf91d78b4ad855300479
kvparth <83730079+kvparth@users.noreply.github.com>
2022-10-01 17:35:11 +0530

    Update SnakeGameApplication.java

--- src/main/java/SnakeAndLadder/SnakeGame/SnakeGameApplication.java
+++ src/main/java/SnakeAndLadder/SnakeGame/SnakeGameApplication.java
@@ -16,19 +16,19 @@ public class SnakeGameApplication {
 	static Map <Integer,Integer> saanp = new HashMap<>();
 	static Map<Integer,Integer> sidhi= new HashMap<>();
 	{
-		saanp.put(99,54);
+		saanp.put(99,64);
 		saanp.put(96,84);
 		saanp.put(91,34);
 		saanp.put(84,73);
 		saanp.put(62,42);
 		saanp.put(46,36);
-		saanp.put(32,10);
+		saanp.put(34,10);
 		saanp.put(22,15);
 
-		sidhi.put(9,25);
+		sidhi.put(10,25);
 		sidhi.put(18,37);
 		sidhi.put(32,66);
-		sidhi.put(42,88);
+		sidhi.put(56,88);
 		sidhi.put(68,93);
 		sidhi.put(77,88);
 		sidhi.put(22,74);
@@ -81,7 +81,7 @@ public class SnakeGameApplication {
 				playerOne= calculatePlayerPoint(playerOne,diceCount);
 				System.out.println(player1+"::" +playerOne);
 				System.out.println(player2+"::"+playerTwo);
-				System.out.println("-------------------------------------");
+				System.out.println("-**-**---**---**---**------**----");
 				if(isWon(playerOne)){
 					System.out.println(player1+"won the game !!! congo");
 					return;
@@ -91,7 +91,7 @@ public class SnakeGameApplication {
 				playerTwo=calculatePlayerPoint(playerTwo,diceCount);
 				System.out.println(player1+"::" +playerOne);
 				System.out.println(player2+"::"+playerTwo);
-				System.out.println("-------------------------------------");
+				System.out.println("-**-**---**---**---**------**----");
 				if(isWon(playerTwo)){
 					System.out.println("Congo!!!"+player2+"won the game");
 					return;
